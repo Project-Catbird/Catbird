45d7d5b26a5bca1481be893a6121e4369982b668
Christian Stutzman <christian.stutzman01@gmail.com>
2021-07-21 12:14:30 -0700

    Added new directories for organizational purposes, added static hosting via express to host index.html on server

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,1 +1,5 @@
-// random comment
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ var app = express();
 
 var PORT = 3000;
 
+app.use(express.static('./client/dist/'));
+
 
 app.listen(PORT, () => {
   console.log(`server listening on Port: ${PORT}`);
