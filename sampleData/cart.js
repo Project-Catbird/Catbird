def83c8f8670e40e983aec0c178ca95b9306f1b7
Christian Stutzman <christian.stutzman01@gmail.com>
2021-07-21 14:59:07 -0700

    populated sampleData directory with sample data to use to project

---
+++ sampleData/cart.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+  // GET /cart
+  cart: [
+    {
+      "sku_id": 1,
+      "count": 2
+    },
+    {
+      "sku_id": 3,
+      "count": 1
+    },
+    {
+      "sku_id": 5,
+      "count": 33
+    }
+    //...
+  ]
+}
