a77ec3b004b6950b906453f144a4580d228a4159
Louis McGruder <maenkindinc@gmail.com>
2021-07-20 17:31:26 -0700

    Catbird - implemented webpack.config.js and package.json and app server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+var PORT = 3000;
+
+
+app.listen(PORT, () => {
+  console.log(`server listening on Port: ${PORT}`);
+})
+
+
+
+
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+var SRC_DIR = path.join(__dirname, '/client/src');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react"
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
