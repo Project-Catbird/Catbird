ff2514cb9e564624a346e37ee4d6d0403998eaa0
Christian Stutzman <christian.stutzman01@gmail.com>
2021-07-21 15:54:51 -0700

    added folder and file organization to client/src/components directory

---
+++ client/src/components/questions-answers/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+// Import modules to use in component creation, uncomment as neccessary
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// import QuestionsList from './QuestionsList.jsx';
+// import IndividualQuestion from './individual-question/Index/jsx';
+// import SearchQuestions from './SearchQuestions.jsx';
+// import MoreAnsweredQuestions from './MoreAnsweredQuestions.jsx';
+// import AddQuestionModel from './add-question-model/Index.jsx';
+// import AddAnswerModel from './add-answer-model/Index.jsx';
---
+++ client/src/components/ratings-reviews/Index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+// Import modules to use in component creation, uncomment as neccessary
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// import ReviewsList from './ReviewsList.jsx';
+// import IndividualReviewTile from './IndividualReviewTile.jsx';
+// import Sort from './Sort.jsx';
+// import RatingBreakdown from './RatingBreakdown.jsx';
+// import Recommendations from './Recommendations.jsx';
+// import ProductBreakdown from './ProductBreakdown.jsx';
+// import NewReview from './new-review/Index.jsx';
+// import KeywordSearch from './KeywordSearch.jsx';
