d66692451e3b6053fe4f029a1585818b48cc6641
Kenneth Hsu <kenneth@keihsu.dev>
2021-07-23 11:30:33 -0700

    Updated to apply Thunk middleware to store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,14 @@
-import { createStore } from 'redux';
-import rootReducer from './reducers/rootReducer.js';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers/main.js';
 
+const middleware = [thunk];
 
 const initialState = {
   //TODO: set initial state
 };
 
-const store = createStore(rootReducer, initialState);
-
+const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
 
 export default store;
