bb4f0acf94a204f267e6ee1d10fab84af0211d6e
Gulina Tayier <81036602+gulgina84@users.noreply.github.com>
2021-07-24 20:36:14 -0700

    change answers font size and color

--- client/src/components/questions-answers/individual-question/Answer.jsx
+++ client/src/components/questions-answers/individual-question/Answer.jsx
@@ -11,11 +11,11 @@ const Answer = ( { answer, question_id, question_body } ) => {
   <Container>
     <p><strong>A:  </strong>{answer.body}</p>
     <Row>
-      <Col>by {answer.answerer_name}</Col>
-      <Col><Moment format="MMM Do YYYY">{answer.date}</Moment></Col>
-      <Col>Helpful? Yes ({answer.helpfulness})</Col>
+      <Col className="answerStamp">by {answer.answerer_name}</Col>
+      <Col className="answerStamp"><Moment format="MMM Do YYYY">{answer.date}</Moment></Col>
+      <Col className="answerStamp">Helpful? Yes ({answer.helpfulness})</Col>
       <Col></Col>
-      <Col><AddAnswer question_id={question_id} question_body={question_body}/></Col>
+      <Col className="answerStamp"><AddAnswer question_id={question_id} question_body={question_body}/></Col>
     </Row>
   </Container>
 
