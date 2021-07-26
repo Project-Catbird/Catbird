76af878f7ca0db81325ad9d15eefd15abd8039c2
Andy Vo <andyqvo@gmail.com>
2021-07-26 11:19:49 -0700

    Implemented style selectors to include checkmark after clicked

--- client/src/components/overview/StyleSelector.jsx
+++ client/src/components/overview/StyleSelector.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Col, Row, Container, Image } from 'react-bootstrap';
+import { Col, Row, Container, Image, Form } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { setStyle } from '../../redux/actions/productAction';
 
@@ -7,8 +7,16 @@ function StyleSelector(props) {
   const styles = useSelector((state) => state.styles.results);
   const dispatch = useDispatch();
 
+
   const styleComponents = styles.map((style, index) => {
-    return <Col key={index}><Image key={index} src={style.photos[0].thumbnail_url} style_id={style.style_id} width="50px" height="50px" onClick={() => {dispatch(setStyle(style))}}roundedCircle/></Col>
+    return (
+      <Col key={index}>
+      <input type="radio" name="style" id={index} onClick={() => {dispatch(setStyle(style))}}/>
+      <label className="form-check-label" for={index}>
+      <Image key={index} className="style-thumbnail" src={style.photos[0].thumbnail_url} style_id={style.style_id} width="50px" height="50px" roundedCircle/>
+      </label>
+      </Col>
+    )
   })
 
   return (
