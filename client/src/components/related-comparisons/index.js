61ed89c833aa56b40ba5a23294beab83b10b73ae
Kenneth Hsu <kenneth@keihsu.dev>
2021-07-27 15:29:15 -0700

    Added exports of RelatedProducts and OutfitList

---
+++ client/src/components/related-comparisons/index.js
@@ -0,0 +1,11 @@
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+// Import modules to use in component creation, uncomment as neccessary
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// import RelatedProductCards from './related-product-cards/Index.jsx';
+// import RelatedProductsList from './related-products-list/Index.jsx';
+// import OutfitList from './outfit-list/Index.jsx';
+
+export { default as RelatedProducts } from './RelatedProducts.jsx';
+export { default as OutfitList } from './OutfitList.jsx';
