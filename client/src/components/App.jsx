5ac1a53c21b49c50aa0e2432c26150fd2d0482f0
Christian Stutzman <christian.stutzman01@gmail.com>
2021-07-26 20:31:40 -0700

    removed RatingsReviews component from App component to make merging easier

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import RatingsReviews from './ratings-reviews/Index.jsx';
 
+
 // test
-const App = (props) => {
-  return (
-    <RatingsReviews />
-  )
-};
 // const App = (props) => {
 //   return (
-//     <div>Hello World!</div>
+//     <RatingsReviews />
 //   )
 // };
+const App = (props) => {
+  return (
+    <div>Hello World!</div>
+  )
+};
 
 export default App;
