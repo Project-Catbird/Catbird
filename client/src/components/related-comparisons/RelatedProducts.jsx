24d3d1ac26d79ccc83f6b8891ff552755303e42c
Kenneth Hsu <kenneth@keihsu.dev>
2021-07-27 15:28:24 -0700

    Completed Related Products container using static product id for testing

---
+++ client/src/components/related-comparisons/RelatedProducts.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import ProductCard from './ProductCard.jsx';
+import { CardGroup } from 'react-bootstrap';
+import axios from 'axios';
+import { API_KEY, API_URL } from '../../config/config.js';
+
+const RelatedProducts = (props) => {
+  //Temporary product id. Replace with current product from state.
+  const productId = 16056;
+
+  const [relatedProducts, setRelatedProducts] = useState([]);
+  useEffect(() => {
+    axios.get(`${API_URL}/products/${productId}/related`, {
+      headers: {
+        Authorization: API_KEY
+      }
+    })
+    .then((response) => {
+      setRelatedProducts(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }, []);
+
+  const relatedProductList = relatedProducts.map((productId) =>
+    <ProductCard key={productId} productId={productId} cardType="related"/>
+  )
+  return(
+    <div>
+      <div>
+        <h2>Related Products</h2>
+      </div>
+      { relatedProducts.length > 0 &&
+        <CardGroup className='d-flex flex-nowrap containers'>
+          {relatedProductList}
+        </CardGroup>
+      }
+    </div>
+  )
+}
+
+export default RelatedProducts;
