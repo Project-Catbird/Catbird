d2ef540239a96232a805737c8e6644360cf6e449
Kenneth Hsu <kenneth@keihsu.dev>
2021-07-27 15:33:41 -0700

    Removed unused directories to reduce clutter

--- client/src/components/related-comparisons/Index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-////////////////////////////////////////////////////////////////////////////////////////////////
-// Import modules to use in component creation, uncomment as neccessary
-////////////////////////////////////////////////////////////////////////////////////////////////
-
-// import RelatedProductCards from './related-product-cards/Index.jsx';
-// import RelatedProductsList from './related-products-list/Index.jsx';
-// import OutfitList from './outfit-list/Index.jsx';
