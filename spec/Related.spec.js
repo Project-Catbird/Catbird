b6b7cec23db38d4a33ddb06108d86689e109e70e
Kenneth Hsu <kenneth@keihsu.dev>
2021-08-02 16:43:39 -0700

    Started writing tests

---
+++ spec/Related.spec.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { connect, Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import RelatedComparisons from '../client/src/components/related-comparisons/Index.jsx';
